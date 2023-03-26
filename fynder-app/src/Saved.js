94e3b87c780c96c166514b1426bdadde2d7e7d56
Sean Stevenson <sean.flynn.stevenson@gmail.com>
2023-03-26 20:52:18 +0100

    Added text to cards

--- fynder-app/src/Saved.js
+++ fynder-app/src/Saved.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import { IconButton } from '@mui/material';
+import "./Saved.css"
 
 function Saved() {
     return (
