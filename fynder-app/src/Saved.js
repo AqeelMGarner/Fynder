078f93652a763eb7b8b12f3eaf4b26b62d26b8fa
Alhassan Dumbuya <alhassandumbuya@Alhassans-MacBook-Air.local>
2023-03-27 18:29:34 +0100

    added function to get stored data from local storage

--- fynder-app/src/Saved.js
+++ fynder-app/src/Saved.js
@@ -3,7 +3,15 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import { IconButton } from '@mui/material';
 import "./Saved.css"
 
-function Saved() { 
+function Saved() {
+   const [places, setItems] = useState([]);
+
+   useEffect(() => {
+     const places = JSON.parse(localStorage.getItem("places"));
+     if (places) {
+       setItems(places);
+     }
+   }, []);
     const savedPlaces = () => {}
     return (
         <div>
