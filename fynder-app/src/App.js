0ea9aeda9bb9fe74efebd50ad713acde90494906
aqeel garner <aqeel.garner1@gmail.com>
2023-03-30 18:28:53 +0100

    clean code in app.js, sidebar.css, sidebar.js

--- fynder-app/src/App.js
+++ fynder-app/src/App.js
@@ -11,7 +11,6 @@ function App() {
   const [placesInfo, setPlacesInfo] = useState([]);
   // const testPlacesInfo = [{ name: 'Place 1', url: 'https://place1.com' }, { name: 'Place 2', url: 'https://place2.com' }, { name: 'Place 3', url: 'https://place3.com' }];
 
-
   return (
     <div className="App">
       <GlobalProvider>
@@ -20,14 +19,10 @@ function App() {
         {/* Search bar */}
         <SearchBar />
 
-
         <div className="body">
 
           {/* Tinder Cards */}
           <PlaceCards />
-
-
-
 
         </div>
         <Sidebar />
